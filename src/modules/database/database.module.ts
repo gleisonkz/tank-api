8cdf09184f9b08c45d14ec2368a6029643006a76
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-13 11:51:11 -0300

    📦 refactor: remove port

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -8,7 +8,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5432,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
