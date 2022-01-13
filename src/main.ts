4be97feb73b0561643ae7a3db426daf067e7f42d
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-13 10:42:08 -0300

    ✨ feat: swagger and validation pipe

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix('/api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Tank Api')
+    .setDescription('Sample API built to learn about Nest.js')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
